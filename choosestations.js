ef503ffcde7086306e1faad5a9b7173fb50f32fa
Jeff Hamill <jthamill@gmail.com>
2023-02-26 17:24:15 -0500

    choose from array and push to new one

---
+++ choosestations.js
@@ -0,0 +1,12 @@
+const stations = [["Big Bear Donair", 10, "restaurant"], ["Bright Lights Elementary", 50, "school"], ["Moose Mountain Community Center", 45, "community center"]]; let suitableStations = [];
+
+const chooseStations = function (stations) {
+  for (let i = 0; i <= stations.length-1; i++) {
+    if (stations[i][1] >= 20  && (stations[i][2] === "school" || stations[i][2] === "community center")) { 
+      suitableStations.push(stations[i][0]);    
+    }
+  }
+  return suitableStations;
+}
+  
+console.log(chooseStations(stations));
