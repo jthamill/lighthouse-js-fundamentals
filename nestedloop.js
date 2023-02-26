37e30ac4f4e758eb2afb9ede168e9ed4edd20060
Jeff Hamill <jthamill@gmail.com>
2023-02-25 19:52:54 -0500

    nested loop exercise

---
+++ nestedloop.js
@@ -0,0 +1,8 @@
+// print out row and seat combo for a theater wirh rows 0-25 and seats 0-99
+
+
+
+for (let row = 0; row <= 25; row++) {
+  for (let seat = 0; seat <= 99; seat++){
+      console.log(row+"-"+seat);
+      }
