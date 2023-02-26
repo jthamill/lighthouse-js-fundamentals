59a6a4df58dd631605e7c0fa112738f117982900
Jeff Hamill <jthamill@gmail.com>
2023-02-26 13:41:58 -0500

    adding hello.js testing function

---
+++ inlinefunc.js
@@ -0,0 +1,20 @@
+
+
+emotions ("happy", function(laughs) {
+  let numha = "";
+  for (let i = 1; i <= laughs; i++) {
+    numha += "ha";
+  }
+  return numha +"!";
+});
+
+
+function emotions (hahas) {
+   let numha = "";
+   for (let i = 1; i <= hahas; i++) {
+       numha += "ha";
+     }
+     return numha +"!";
+   }
+
+  
