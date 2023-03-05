679dec2b6106a8ed0f3866c362fab816eecd3246
Jeff Hamill <jthamill@gmail.com>
2023-03-05 16:42:07 -0500

    loopylighthouse stretch exercise

--- loopylighthouse.js
+++ loopylighthouse.js
@@ -8,18 +8,21 @@
 
 
 //nice job
-
-for (let n = 100; n <=200; n++) {
-  if (n % 3 === 0 && n % 4 ===0) {
-    console.log("LoopyLighthouse");
-  }
-  else if (n % 3 === 0) {
-    console.log("Loopy");
-  }
-  else if (n % 4 ===0) {
-    console.log("Lighthouse");
-  }
-  else {
-    console.log(n);
+function loopyLighthouse(range, multiples, words) {
+  for (let n = range[0]; n <= range[range.length - 1]; n++) {
+    if (n % multiples[0] === 0 && n % multiples[1] === 0) {
+      console.log(words[0]+" "+words[1]);
+    }
+    else if (n % multiples[0] === 0) {
+      console.log(words[0]);
+    }
+    else if (n % multiples[1] === 0) {
+      console.log(words[1]);
+    }
+    else {
+      console.log(n);
+    }
   }
 }
+
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
