6e1a06cf4ec34cdaad9a8e5b759b872b39d97ee7
Jeff Hamill <jthamill@gmail.com>
2023-03-05 20:21:41 -0500

    merge arrays stretch exercise

---
+++ merge-arrays.js
@@ -0,0 +1,14 @@
+//The function should, when given two sorted arrays containing numbers, returns a sorted array of the numbers from both lists.
+
+function merge(array1, array2) {
+    for (let i = 0; i < array2.length; i++) {
+      array1.push(array2[i]);
+    }
+  array1.sort();  
+  return array1;
+}
+
+
+console.log(merge([4, 5, 6], [1, 2, 3, 4]));
+console.log(merge([4], [2, 5, 8]));
+console.log(merge([1, 2, 6 ], []));
